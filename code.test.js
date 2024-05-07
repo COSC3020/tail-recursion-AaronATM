8fca011aa74b9e8d5f1f67371002c2cde949044a
Aaron Krapes <aaronkrapes@outlook.com>
2024-05-06 21:27:44 -0600

    updated testing code

--- code.test.js
+++ code.test.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const assert = require('assert');
 
-eval(fs.readFileSync('code.js') + '');
+eval(fs.readFileSync('code.js')+'');
 
-assert(fib(15) === 610);
+assert(fib(15) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]);
