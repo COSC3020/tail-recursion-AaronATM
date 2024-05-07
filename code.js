1a16271f907e54e80043dd31582c20c7980c2cd6
Aaron Krapes <aaronkrapes@outlook.com>
2024-05-06 21:27:37 -0600

    updated code.js

---
+++ code.js
@@ -0,0 +1,28 @@
+// took a look at my previous fibonacci sequence program
+
+var fibSeq = [];
+
+function fib(n, i = 2) 
+{
+    if (n === 0)
+    {
+        fibSeq.pop();
+        return fibSeq;
+    }
+    if (n >= 1)
+    {
+        fibSeq[0] = 0;
+    }
+
+    if (n >= 2)
+    {
+        fibSeq[1] = 1;
+    }
+
+    fibSeq.push(fibSeq[i - 1] + fibSeq[i - 2]);
+    n--;
+    i++;
+    return fib(n, i);
+}
+
+console.log(fib(15));
