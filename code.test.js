31d4856bf59e99424aeb7ca70695cf2cc09b8b7d
Aaron Krapes <aaronkrapes@outlook.com>
2024-05-07 09:57:09 -0600

    updated test code

--- code.test.js
+++ code.test.js
@@ -4,4 +4,4 @@ const arr1 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610];
 
 eval(fs.readFileSync('code.js')+'');
 
-assert(JSON.stringify(fib(15)) == JSON.stringify(arr1));
+assert(JSON.stringify(fib(15))==JSON.stringify(arr1));
